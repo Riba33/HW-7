6bb92eadd7089d4c7c182989a7ed4808f38f059c
Riba33 <78846229+Riba33@users.noreply.github.com>
2021-05-04 15:09:29 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,18 +3,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Shape> shapes = new ArrayList<>();
-        shapes.add(new Circle(7,"Circle"));
-        shapes.add(new Quad(5,"Quad"));
-        shapes.add(new Triangle(10,7,8,"Triangle"));
-        shapes.add(new Trapezoid(25,17,8,"Trapezoid"));
-        shapes.add(new Rectangle(13,15,"Rectangle"));
-        printShape(shapes);
-    }
-
-    public static void printShape(ArrayList<Shape> shapes) {
+        shapes.add(new Circle(7, "Circle"));
+        shapes.add(new Quad(5, "Quad"));
+        shapes.add(new Triangle(10, 7, 8, "Triangle"));
+        shapes.add(new Trapezoid(25, 17, 8, "Trapezoid"));
+        shapes.add(new Rectangle(13, 15, "Rectangle"));
         for (Shape shape: shapes) {
-            shape.printShape();
-            System.out.println("Shape square is " + shape.getSquare());
+            ShapeService.printShape(shape);
         }
     }
+
 }
